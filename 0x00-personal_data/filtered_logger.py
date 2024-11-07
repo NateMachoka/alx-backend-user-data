f8015f957b2570f3baa9bd7eafe43937b02c05e8
NateMachoka <natemachoka@gmail.com>
2024-11-07 06:04:03 -0800

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,10 @@ Module that provides functions for filtering log data.
 import re
 from typing import List
 
+
 def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str) -> str:
+        fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """
     Obfuscates specific fields in a log message.
     Args:
