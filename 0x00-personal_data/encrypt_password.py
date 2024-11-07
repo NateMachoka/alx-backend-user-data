57b335e63bd231086c8c61fe24f214b6be24e9c9
NateMachoka <natemachoka@gmail.com>
2024-11-07 08:07:04 -0800

    encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""Contains excrypting functions"""
+
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password with a randomly generated salt.
+    Args:
+        password (str): The password to hash.
+    Returns:
+        bytes: The salted, hashed password.
+    """
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password with the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
