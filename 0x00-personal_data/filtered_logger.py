2a1b760ce759b32ce1e70a4bd2244c3279186ec8
NateMachoka <natemachoka@gmail.com>
2024-11-07 06:18:55 -0800

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-Module that provides functions for filtering log data.
+Module for logging with data redaction.
 """
 
-import re
+import logging
 from typing import List
+import re
 
 
 def filter_datum(
@@ -23,3 +24,35 @@ def filter_datum(
     pattern = '|'.join([f"{field}=[^;]*" for field in fields])
     return re.sub(
         pattern, lambda x: f"{x.group().split('=')[0]}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class to filter sensitive info in log messages.
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the formatter with specific fields to redact.
+
+        Args:
+            fields (List[str]): List of field names to redact.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record to redact specified fields.
+        Args:
+            record (logging.LogRecord): The log record to format.
+        Returns:
+            str: The formatted and redacted log message.
+        """
+        original_message = super().format(record)
+        return filter_datum(
+            self.fields, self.REDACTION, original_message, self.SEPARATOR)
