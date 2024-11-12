5673740ee29379067759f5aff0fc0b5f5b925a6f
NateMachoka <natemachoka@gmail.com>
2024-11-12 11:51:19 -0800

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth 
 import os
 
 
@@ -17,7 +18,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 # Initialize `auth` based on the environment variable `AUTH_TYPE`
 auth = None
-if getenv("AUTH_TYPE") == "auth":
+auth_type = getenv("AUTH_TYPE")
+
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+elif auth_type == "auth":
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic Authentication class"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class for basic authentication """
+    pass
