bb06ccffc0685326d059c6718c393aec8face702
NateMachoka <natemachoka@gmail.com>
2024-11-14 01:54:05 -0800

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module for handling session-based authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session authentication class that inherits from Aut
+    """
+    pass
