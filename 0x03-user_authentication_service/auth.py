0f02672f556b1d85506967397dab9d70217b6dc2
NateMachoka <natemachoka@gmail.com>
2024-11-21 02:43:20 -0800

    .py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,6 +23,10 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+def _generate_uuid(self) -> str:
+        """Generate a new UUID."""
+        return str(uuid.uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database."""
