975e8d4af2f85aab038c0493f7bd4b345b8da4e9
NateMachoka <natemachoka@gmail.com>
2024-11-21 00:13:14 -0800

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ DB module
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from sqlalchemy.orm.session import Session
 from user import Base, User
 
@@ -42,4 +43,31 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user in the database by arbitrary keyword arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for filtering.
+
+        Returns:
+            User: The first user found matching the criteria.
+
+        Raises:
+            NoResultFound: If no user matches the criteria.
+            InvalidRequestError: If the query arguments are invalid.
+        """
+        # Define valid keys corresponding to User model attributes
+        valid_keys = (
+            'id', 'email', 'hashed_password', 'session_id', 'reset_token')
+        for key in kwargs.keys():
+            if key not in valid_keys:
+                raise InvalidRequestError
+
+        # Perform the query and handle no result scenario
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
         return user
