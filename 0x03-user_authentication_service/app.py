bbd2e7884f30686cc28b842f82d4a17e58868599
NateMachoka <natemachoka@gmail.com>
2024-11-21 01:42:40 -0800

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,12 +6,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def welcome():
     """
     Root route returning a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
