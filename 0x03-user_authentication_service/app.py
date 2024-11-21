68d2a74c1f860a6164a831c3eaeb65876442114e
NateMachoka <natemachoka@gmail.com>
2024-11-21 08:06:27 -0800

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,11 +2,12 @@
 """
 Basic Flask app with a single route.
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
-AUTH = Auth() 
+AUTH = Auth()
+
 
 @app.route("/", methods=["GET"])
 def welcome():
@@ -35,6 +36,34 @@ def users() -> str:
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    POST /sessions route to log in the user and create a session.
+
+    Expected form data:
+        - email: User's email address
+        - password: User's password
+
+    Returns:
+        - JSON payload with the session ID if the login is successful.
+        - Abort with 401 if login information is incorrect.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Check if the login credentials are valid
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie('session_id', session_id)
+
+    return response
 
 
 if __name__ == "__main__":
