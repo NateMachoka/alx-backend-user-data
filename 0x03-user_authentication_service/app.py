0f4b1f7e348439c721d281b77fa3f9df8bc90803
NateMachoka <natemachoka@gmail.com>
2024-11-21 02:00:39 -0800

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,5 +15,26 @@ def welcome():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """
+    POST /users route to register a new user.
+
+    Expected form data:
+        - email: User's email address
+        - password: User's password
+
+    Returns:
+        - JSON payload with a success or error message.
+    """
+    try:
+        email = request.form.get('email', None)
+        password = request.form.get('password', None)
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
